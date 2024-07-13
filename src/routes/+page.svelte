d7044dabb331801e9d390d3149bb98b43b74f62e
Lavra <lavratamutus@gmail.com>
2024-07-13 17:46:05 -0400

    Workflow configs and trivial homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,12 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Moneyless Society Hub</h1>
+<h2>Jump into action</h2>
+<p>
+	MoSo's Technomancy Circle has begun to work on an app for organizing our action. What should it
+	have?
+</p>
+<h3>Ideas so far</h3>
+<ul>
+	<li>Roster</li>
+	<li>Quick links</li>
+	<li>Interactive diagram of our circles</li>
+</ul>
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
+import { viteStaticCopy } from "vite-plugin-static-copy"
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [sveltekit(),viteStaticCopy({
+        targets: []
+      })],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
